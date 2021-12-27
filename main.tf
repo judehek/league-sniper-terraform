d7ef6121fe5f5eccb73329f0264df075c66c03d5
GeckoThePecko <bernie.gandin2@gmail.com>
2021-12-27 16:43:35 -0500

    Initial terraform commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "doordash_accounts" {
+  hash_key = "email"
+  name = "doordash_accounts"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "email"
+    type = "S"
+  }
+  attribute {
+    name = "password"
+    type = "S"
+  }
+  attribute {
+    name = "date_created"
+    type = "S"
+  }
+}
