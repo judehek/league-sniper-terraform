b6cad62f47df8b1dfce28c024c1f6c43b15e277e
GeckoThePecko <bernie.gandin2@gmail.com>
2021-12-27 16:57:42 -0500

    Fixed attributes

--- main.tf
+++ main.tf
@@ -11,11 +11,12 @@ resource "aws_dynamodb_table" "doordash_accounts" {
     type = "S"
   }
   attribute {
-    name = "password"
-    type = "S"
-  }
-  attribute {
     name = "date_created"
     type = "S"
+  }
+  global_secondary_index {
+    name = "DateCreatedIndex"
+    hash_key = "date_created"
+    projection_type = "ALL"
   }
 }
