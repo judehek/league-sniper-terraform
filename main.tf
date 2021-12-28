66925933d3a38cba915e21785370f2c44d396687
Bernie G <bernie.gandin2@gmail.com>
2021-12-27 19:54:06 -0500

    Add ecr lifecycle policy to delete containers when there's more than 1 (#2)
    * Add ecr lifecycle policy to delete containers when there's more than 1
    
    * Fix string

--- main.tf
+++ main.tf
@@ -24,3 +24,9 @@ resource "aws_dynamodb_table" "doordash_accounts" {
 resource "aws_ecr_repository" "doordash_account_creator" {
   name = "doordash_account_creator"
 }
+
+resource "aws_ecr_lifecycle_policy" "doordash_account_creator_registry_lifecycle_policy" {
+  policy = var.doordash_ecr_lifecycle_policy
+  repository = aws_ecr_repository.doordash_account_creator.id
+}
+
--- variables.tf
+++ variables.tf
@@ -1,4 +1,25 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-east-2"
+  default = "us-east-2"
+}
+
+variable "doordash_ecr_lifecycle_policy" {
+  default = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Rule 1",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
 }
