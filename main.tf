54a29b61fa9096a781d66c1870ce0f693b71daa0
GeckoThePecko <bernie.gandin2@gmail.com>
2021-12-28 19:54:32 -0500

    region

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_dynamodb_table" "doordash_accounts" {
 }
 
 resource "aws_default_subnet" "default_aws_subnet" {
-  availability_zone = "us-east-2"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "Default subnet for us-east-2"
