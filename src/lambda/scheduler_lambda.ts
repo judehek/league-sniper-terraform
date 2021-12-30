c2d7eff88309bb1f2e9b5edb9684540b41f55baa
GeckoThePecko <bernie.gandin2@gmail.com>
2021-12-29 23:10:22 -0500

    Log event body

--- src/lambda/scheduler_lambda.ts
+++ src/lambda/scheduler_lambda.ts
@@ -1,11 +1,21 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import AWS from 'aws-sdk';
 
 export const handler = async (
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-    console.log("yo yo!")
+    let parsed = JSON.parse(event.body);
+    console.log(event.body)
+    if (parsed) {
+        var ecs = new AWS.ECS();
+
+        return {
+            statusCode: 200,
+            body: "Successfully started ECS task!"
+        }
+    }
     return {
-        statusCode: 200,
-        body: `Heyo`
+        statusCode: 500,
+        body: "Could not parse input body!"
     }
 }
