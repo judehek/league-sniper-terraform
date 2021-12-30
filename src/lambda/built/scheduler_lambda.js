235414d1a51cd8b452878a6c0caa93a0db43c818
GeckoThePecko <bernie.gandin2@gmail.com>
2021-12-29 22:07:35 -0500

    Add scheduler to git ig

---
+++ src/lambda/built/scheduler_lambda.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+exports.handler = async (event) => {
+    console.log("yo yo!");
+    return {
+        statusCode: 200,
+        body: `Heyo`
+    };
+};
+//# sourceMappingURL=scheduler_lambda.js.map
