35fce8c20cab0bc0cb2340e793e9deab9b374d6a
GeckoThePecko <bernie.gandin2@gmail.com>
2021-12-29 23:12:04 -0500

    commited too early rip

--- src/lambda/built/scheduler_lambda.js
+++ src/lambda/built/scheduler_lambda.js
@@ -1,11 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
+const aws_sdk_1 = require("aws-sdk");
 exports.handler = async (event) => {
-    console.log("yo yo!");
+    let parsed = JSON.parse(event.body);
+    console.log(event.body);
+    if (parsed) {
+        var ecs = new aws_sdk_1.default.ECS();
+        return {
+            statusCode: 200,
+            body: "Successfully started ECS task!"
+        };
+    }
     return {
-        statusCode: 200,
-        body: `Heyo`
+        statusCode: 500,
+        body: "Could not parse input body!"
     };
 };
 //# sourceMappingURL=scheduler_lambda.js.map
