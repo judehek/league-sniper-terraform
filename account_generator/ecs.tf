dd7f27ed529b98d8370ef0fd4684960bca220a0d
GeckoThePecko <bernie.gandin2@gmail.com>
2022-02-06 23:55:34 -0500

    ok this is too much work imma just hardcode the arn

--- account_generator/ecs.tf
+++ account_generator/ecs.tf
@@ -2,22 +2,6 @@ resource "aws_cloudwatch_log_group" "doordash_account_generator_log_group" {
   name = "doordash_account_generator_log_group"
 }
 
-resource "aws_iam_role" "doordash_account_generator_role" {
-  name               = "doordash_account_generator_role"
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Action = "dynamodb:*"
-        Effect = "Allow"
-        Principal: {
-          Service: "ec2.amazonaws.com"
-        },
-      },
-    ]
-  })
-}
-
 resource "aws_ecs_task_definition" "doordash_account_generator_ecs_task_definition" {
   container_definitions    = var.doordash_account_generator_ecs_definition
   family                   = "doordash-account-generator"
@@ -28,7 +12,7 @@ resource "aws_ecs_task_definition" "doordash_account_generator_ecs_task_definiti
   cpu                      = 512
   memory                   = 1024
   execution_role_arn       = var.ecsTaskExecutionRole
-  task_role_arn            = aws_iam_role.doordash_account_generator_role.arn
+  task_role_arn            = "arn:aws:iam::719795077267:role/Doordash_Account_Generator_ECS_Role"
   lifecycle {
     ignore_changes = [
       container_definitions]
