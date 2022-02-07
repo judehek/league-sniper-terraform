21c01200e9c605269465fcd6c7fbe667e7f6cffe
Wateryadoin <judebhekman@gmail.com>
2022-02-07 14:40:09 -0700

    Add indexes (#9)

--- account_generator/dynamo.tf
+++ account_generator/dynamo.tf
@@ -10,9 +10,27 @@ resource "aws_dynamodb_table" "doordash_accounts" {
     name = "date_created"
     type = "S"
   }
+  attribute {
+    name = "is_original"
+    type = "S"
+  }
+  attribute {
+    name = "is_used"
+    type = "S"
+  }
   global_secondary_index {
     name            = "DateCreatedIndex"
     hash_key        = "date_created"
+    projection_type = "ALL"
+  }
+  global_secondary_index {
+    name            = "IsOriginalIndex"
+    hash_key        = "is_original"
+    projection_type = "ALL"
+  }
+  global_secondary_index {
+    name            = "IsUsedIndex"
+    hash_key        = "is_used"
     projection_type = "ALL"
   }
 }
