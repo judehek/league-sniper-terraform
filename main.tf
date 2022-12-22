d088a7d3fda95ad1276c100be589b78d9beb0810
Wateryadoin <judebhekman@gmail.com>
2022-12-22 15:01:32 -0700

    Add role

--- main.tf
+++ main.tf
@@ -8,8 +8,29 @@ data "archive_file" "build_code" {
   output_path = "${path.module}/sniper/output/python.zip"
 }
 
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "sniper_function" {
   count = "3"
+  role = aws_iam_role.lambda_role.arn
   filename = "${path.module}/sniper/output/python.zip"
   function_name = "na-sniper-${count.index + 1}"
   runtime = "python3.9"
